d18bade6ca04e5ad49e84bdc2107f1a37de9f057
Barun Kumar Pal <48109802+barunkumarpal@users.noreply.github.com>
2020-09-11 20:51:13 +0530

    added original files

--- my_custom_testimonial/inc/testimonial_frontend_form.php
+++ my_custom_testimonial/inc/testimonial_frontend_form.php
@@ -1,10 +1,16 @@
 <?php
 function add_testimonial_frontend(){
+    if(!is_user_logged_in()){
+        ?>
+            <h4 class="text-center mb-4 mt-4">Please Login First to give a review. <a href="<?php echo site_url().'/login-form'; ?>">Login here</a></h4>
+        <?php
+    } else{
         $form = file_get_contents('add_testimonial_form.php', true);
         $editor_content = getnerate_form();
         $create_form = str_replace('CONTENT_EDITOR', $editor_content, $form );
         
         return $create_form;
+    }
 }
 
 function getnerate_form(){
